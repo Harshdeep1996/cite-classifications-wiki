1ea8cd672a50622f564aab579d306dff7e9acb6f
Harshdeep <16365803+Harshdeep1996@users.noreply.github.com>
2019-03-07 11:21:02 +0100

    Const file for list of templates

---
+++ const.py
@@ -0,0 +1,30 @@
+# -*- encoding: utf-8 -*-
+
+# Ref: https://github.com/dissemin/wikiciteparser/blob/master/wikiciteparser/en/__init__.py
+
+CITATION_TEMPLATES = set([
+    'citation',
+    'cite arxiv',
+    'cite av media',
+    'cite av media notes',
+    'cite book',
+    'cite conference',
+    'cite dvd notes',
+    'cite encyclopedia',
+    'cite episode',
+    'cite interview',
+    'cite journal',
+    'cite mailing list',
+    'cite map',
+    'cite news',
+    'cite newsgroup',
+    'cite podcast',
+    'cite press release',
+    'cite report',
+    'cite serial',
+    'cite sign',
+    'cite speech',
+    'cite techreport',
+    'cite thesis',
+    'cite web'
+])
