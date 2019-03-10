019b25b80ebc52a0d6b795b81e6f357b0813f167
Harshdeep <harsh.deep@student.manchester.ac.uk>
2019-03-10 13:25:37 +0100

    remove the balanced ref part - will do in step 2

--- get_data.py
+++ get_data.py
@@ -5,7 +5,6 @@ import regex
 from bs4 import BeautifulSoup
 from pyspark.sql import Row
 from io import StringIO
-from helpers import check_if_balanced
 from pyspark import SparkContext, SQLContext
 from pyspark.sql.functions import explode, col
 
@@ -15,11 +14,6 @@ OUTPUT_DATA = 'hdfs:///user/harshdee/citations.parquet'
 CITATION_REGEX = (
     '{{ci[\w\s]*[^}]*}}(?:}}(?R)?)?|{{Ci[\w\s]*[^}]*}}(?:}}(?R)?)?|'
     '{{h[\w\s]*[^}]*}}(?:}}(?R)?)?|{{H[\w\s]*[^}]*}}(?:}}(?R)?)?'
-)
-
-CITATION_UNBALANCED_REGEX = (
-    '{{ci[\w\s]*[^}]*}}(?:[\s]*[^}]*}}(?R)?)?|{{Ci[\w\s]*[^}]*}}(?:[\s]*[^}]*}}(?R)?)?|'
-    '{{h[\w\s]*[^}]*}}(?:[\s]*[^}]*}}(?R)?)?|{{H[\w\s]*[^}]*}}(?:[\s]*[^}]*}}(?R)?)?'
 )
 
 sc = SparkContext()
@@ -32,22 +26,6 @@ pages = wiki.where('ns = 0').where('redirect is null')
 # Get only ID, title, revision text's value which we are interested in
 pages = pages['id', 'title', 'revision.text.#VALUE']
 pages = pages.withColumnRenamed('#VALUE', 'content')
-
-def _replace_unbalanced_refs(line, all_refs_balanced, original_refs):
-    """
-    Check if the curly brackets are referenced in the references and if not
-    apply another regex.
-
-    :param: line: the content in which references are present.
-    :param: all_refs_balanced: boolean list for which refs are balanced or not.
-    :param: original_refs: references which are not balanced.
-    """
-    temp_refs = regex.findall(CITATION_UNBALANCED_REGEX, line)
-    indexes_not_balanced = [i for i, balanced in enumerate(all_refs_balanced) if not balanced]
-
-    for index in indexes_not_balanced:
-        original_refs[indexes_not_balanced[index]] = temp_refs[index]
-    return original_refs
 
 def get_citations(page_content):
     """
@@ -65,10 +43,6 @@ def get_citations(page_content):
             current_section = line.strip()
         elif line.startswith('* {{'):
             refs_in_line = regex.findall(CITATION_REGEX, line)
-            all_refs_balanced = [check_if_balanced(ref) for ref in refs_in_line]
-
-            if not all(all_refs_balanced):
-                refs_in_line = _replace_unbalanced_refs(line, all_refs_balanced, refs_in_line)
         else:
             refs_in_line = [r.text for r in BeautifulSoup(repr(line)).findAll('ref') if r.text]
 
