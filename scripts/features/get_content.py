1922e7076f2d1d20d72c015d851f7aea0de39039
Harshdeep <16365803+Harshdeep1996@users.noreply.github.com>
2019-11-15 23:06:14 +0000

    Adding script for get content

---
+++ scripts/features/get_content.py
@@ -0,0 +1,20 @@
+from pyspark.sql import Row
+from pyspark import SparkContext, SQLContext
+
+INPUT_DATA = 'hdfs:///user/piccardi/enwiki-20181001-pages-articles-multistream.xml.bz2'
+OUTPUT_DATA = 'hdfs:///user/harshdee/citations_content.parquet'
+
+sc = SparkContext()
+sqlContext = SQLContext(sc)
+sqlContext.setConf('spark.sql.parquet.compression.codec', 'snappy')
+
+wiki = sqlContext.read.format('com.databricks.spark.xml').options(rowTag='page').load(INPUT_DATA)
+pages = wiki.where('ns = 0').where('redirect is null')
+
+# Get only ID, title, revision text's value which we are interested in
+pages = pages['id', 'title', 'revision.text.#VALUE']
+pages = pages.toDF('id', 'page_title', 'content')
+
+# citations_with_words = sqlContext.createDataFrame(pages.map(get_as_row))
+pages.write.mode('overwrite').parquet(OUTPUT_DATA)
+# citations_with_words.write.format('com.databricks.spark.csv').save('citations_words.csv')
